aafb90c689f0a963e91620566edfad873fcb7d68
EricBernardi <eric.gomes.bernardi@gmail.com>
2024-04-01 01:03:57 -0300

    FIX: Ajustes nos parametros da rota

--- routes/routes.ts
+++ routes/routes.ts
@@ -5,18 +5,22 @@ const router = express.Router();
 
 router.post(['/', '/:country'], async (req: Request, res: Response) => {
   const country = req.params.country ?? '';
-  const page = req.query.page ? parseInt(req.query.page as string) : 1;
-  const limit = req.query.page ? parseInt(req.query.limit as string) : 10;
+  const page = parseInt(req.query?.page as string);
+  const limit = parseInt(req.query?.limit as string);
 
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
+  // const startIndex = (page - 1) * limit;
+  // const endIndex = page * limit;
 
   const dataCountry = await checkCountries(country);
 
-  const paginatedData = Object.entries(dataCountry).slice(startIndex, endIndex);
+  // const paginatedData = Object.entries(dataCountry).slice(startIndex, endIndex);
+
+  const paginatedData = Object.entries(dataCountry);
 
   res.status(200).send({
     paginatedData,
+    totalPages: Object.entries(dataCountry).length / limit,
+    currentPage: page,
   });
 });
 
